057b8db984dfb71523e08e59be951f48d0e6efb3
Matija Pretnar <matija@pretnar.info>
2026-03-30 08:54:42 +0200

    Dodatni primeri dokazov

--- datoteke/06-dokazovalniki/dokazujmo.ml
+++ datoteke/06-dokazovalniki/dokazujmo.ml
@@ -1,5 +1,15 @@
 type empty
 type ('a, 'b) sum = Inl of 'a | Inr of 'b
+type 'a not = 'a -> empty
+
+(*
+    Dokažimo
+        P ∧ Q ⇒ Q ∧ P
+    tako, da skonstruiramo izraz tipa
+        'a * 'b -> 'b * 'a
+*)
+
+let konjunkcija_komutira (dokaz_a, dokaz_b) = (dokaz_b, dokaz_a)
 
 (*
     Dokažimo
@@ -9,19 +19,30 @@ type ('a, 'b) sum = Inl of 'a | Inr of 'b
 *)
 
 let distributivnost : 'a * ('b, 'c) sum -> ('a * 'b, 'a * 'c) sum =
-    fun (a, b_ali_c) ->
-        match b_ali_c with
-        | Inl b -> Inl (a, b)
-        | Inr c -> Inr (a, c)
+ fun (dokaz_a, dokaz_b_ali_c) ->
+  match dokaz_b_ali_c with
+  | Inl dokaz_b -> Inl (dokaz_a, dokaz_b)
+  | Inr dokaz_c -> Inr (dokaz_a, dokaz_c)
+
+(*
+    Dokažimo
+        ¬P ∨ ¬Q ⇒ ¬(P ∧ Q)
+    tako, da skonstruiramo izraz tipa
+        ('a not, 'b not) sum -> ('a * 'b) not
+*)
+
+let negacija_konjunkcije (dokaz_ne_a_ali_ne_b : ('a not, 'b not) sum) :
+    ('a * 'b) not =
+  match dokaz_ne_a_ali_ne_b with
+  | Inl dokaz_ne_a -> fun (dokaz_a, dokaz_b) -> dokaz_ne_a dokaz_a
+  | Inr dokaz_ne_b -> fun (dokaz_a, dokaz_b) -> dokaz_ne_b dokaz_b
 
 (* 0. "težava": nerodno pisanje dokazov - če se vam ne zdi nerodno, vam bo všeč Agda *)
 
 (* 1. težava: izjeme in divergenca *)
 
-let a_je_vedno_res : 'a = failwith "Boom"
-
-let rec iz_a_sledi_b (dokaz_a : 'a) : 'b =
-    iz_a_sledi_b dokaz_a
+let dokaz_a : 'a = failwith "Boom"
+let rec dokaz_iz_a_sledi_b (dokaz_a : 'a) : 'b = dokaz_iz_a_sledi_b dokaz_a
 
 (* 2. težava: kaj pa predikati? *)
 
