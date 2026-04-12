eaf2cef472974fca94426f86a1025db762dbc687
Matija Pretnar <matija@pretnar.info>
2026-04-12 12:39:28 +0200

    Dokončal implementacijo z intrinzičnimi tipi

--- datoteke/07-mehanizacija/intrinzicni-tipi.lean
+++ datoteke/07-mehanizacija/intrinzicni-tipi.lean
@@ -2,7 +2,7 @@ inductive Ty : Type where
 | Bul : Ty
 | Fun : Ty → Ty → Ty
 
-notation:25 A " ⇒ " B => Ty.Fun A B
+notation A " ⇒ " B => Ty.Fun A B
 
 inductive Ctx : Type where
 | empty : Ctx
@@ -34,21 +34,40 @@ example {A B C} : Tm Ctx.empty ((B ⇒ C) ⇒ (A ⇒ B) ⇒ (A ⇒ C)) :=
         app (var (S (S Z))) (app (var (S Z)) (var Z))
     )))
 
+def extendRename (ρ : {B : Ty} → Var Γ B → Var Δ B) : ({B : Ty} → Var (Ctx.snoc Γ A) B → Var (Ctx.snoc Δ A) B)
+| _, Var.Z => Var.Z
+| _, Var.S y => Var.S (ρ y)
 
--- def subst (x : Nat) (V : Tm) : Tm → Tm
--- | Tm.tru => Tm.tru
--- | Tm.fls => Tm.fls
--- | Tm.ite M N1 N2 => Tm.ite (subst x V M) (subst x V N1) (subst x V N2)
--- | Tm.var y => sorry
--- | Tm.lam M => sorry
--- | Tm.app M N => Tm.app (subst x V M) (subst x V N)
+def rename : ({B : Ty} → Var Γ B → Var Δ B) → {A : Ty} → Tm Γ A → Tm Δ A
+| _, _, Tm.tru => Tm.tru
+| _, _, Tm.fls => Tm.fls
+| ρ, _, Tm.ite M N1 N2 => Tm.ite (rename ρ M) (rename ρ N1) (rename ρ N2)
+| ρ, _, Tm.var y => Tm.var (ρ y)
+| ρ, _, Tm.lam M => Tm.lam (rename (extendRename ρ) M)
+| ρ, _, Tm.app M N => Tm.app (rename ρ M) (rename ρ N)
 
--- inductive IsValue : Tm _ _ → Prop where
--- | tru : IsValue Tm.tru
--- | fls : IsValue Tm.fls
--- | lam : IsValue (Tm.lam _)
+def extendSubst (σ : {B : Ty} → Var Γ B → Tm Δ B) : ({B : Ty} → Var (Ctx.snoc Γ A) B → Tm (Ctx.snoc Δ A) B)
+| _, Var.Z => Tm.var Var.Z
+| _, Var.S y => rename Var.S (σ y)
 
-inductive Step : Tm Γ A → Tm Γ A → Prop where
+def subst : ({B : Ty} → Var Γ B → Tm Δ B) → {A : Ty} → Tm Γ A → Tm Δ A
+| _, _, Tm.tru => Tm.tru
+| _, _, Tm.fls => Tm.fls
+| σ, _, Tm.ite M N1 N2 => Tm.ite (subst σ M) (subst σ N1) (subst σ N2)
+| σ, _, Tm.var y => σ y
+| σ, _, Tm.lam M => Tm.lam (subst (extendSubst σ) M)
+| σ, _, Tm.app M N => Tm.app (subst σ M) (subst σ N)
+
+def substOne (V : Tm Γ A) : {B : Ty} → Var (Ctx.snoc Γ A) B → Tm Γ B
+| _, Var.Z => V
+| _, Var.S y => Tm.var y
+
+inductive IsValue : {Γ : Ctx} → {A : Ty} → Tm Γ A → Prop where
+| tru : IsValue Tm.tru
+| fls : IsValue Tm.fls
+| lam : IsValue (Tm.lam _)
+
+inductive Step : {Γ : Ctx} → {A : Ty} → Tm Γ A → Tm Γ A → Prop where
 | ite_step :
     Step M M' →
     ---------------------------------------
@@ -64,24 +83,24 @@ inductive Step : Tm Γ A → Tm Γ A → Prop where
     -------------------------------
     Step (Tm.app M N) (Tm.app M' N)
 | app_step2 :
-    -- IsValue V →
+    IsValue V →
     Step N N' →
     -------------------------------
     Step (Tm.app V N) (Tm.app V N')
--- | app_lam :
---     IsValue V →
---     ----------------------------------------
---     Step (Tm.app (Tm.lam M) V) (subst 0 V M)
+| app_lam :
+    IsValue V →
+    ----------------------------------------
+    Step (Tm.app (Tm.lam M) V) (subst (substOne V) M)
 
 set_option pp.fieldNotation false
 
-inductive Progresses : Tm → Prop where
+inductive Progresses : {Γ : Ctx} → {A : Ty} → Tm Γ A → Prop where
 | step : Step M M' → Progresses M
 | value : IsValue M → Progresses M
 
 theorem progress :
     Γ = Ctx.empty →
-    HasType Γ M A →
+    (M : Tm Γ A) →
     Progresses M :=
 by
     intro h_ctx h_typ
@@ -100,11 +119,27 @@ by
                 exact Progresses.step (Step.ite_tru)
             case fls =>
                 exact Progresses.step (Step.ite_fls)
-            case lam =>
-                contradiction
     case var =>
         rename_i tezava
         rewrite [h_ctx] at tezava
-        cases tezava
-    sorry
-    sorry
+        contradiction
+    case lam =>
+        exact Progresses.value IsValue.lam
+    case app A B M N M_ih N_ih =>
+        cases M_ih h_ctx
+        case step =>
+            apply Progresses.step
+            apply Step.app_step1
+            assumption
+        case value M_val =>
+            cases N_ih h_ctx
+            case step =>
+                apply Progresses.step
+                apply Step.app_step2
+                · assumption
+                · assumption
+            case value =>
+                cases M_val
+                apply Progresses.step
+                apply Step.app_lam
+                assumption
